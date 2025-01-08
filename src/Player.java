6a4cb7ae488b5ec49ae70898dbd0bc970d63bc53
Aizeia <bobedibabedi@gmail.com>
2025-01-08 14:06:05 +0100

    Player Hp,atk,etc

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,7 @@
 public class Player {
-    private String name;
+    private final String name;
+    private int health;
+    private int attackPower;
     // Metod för att skriva in sitt namn
     public Player(String name) {
         this.name = name;
@@ -7,5 +9,28 @@ public class Player {
     // Returnerar en String med värdet name
     public String getName() {
         return name;
+    }
+
+    public Player(String name, int health, int attackPower){
+        this.name = name;
+        this.attackPower = attackPower;
+        this.health = health;
+    }
+
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
     }
 }
