878160ebc1f50750ed9262a9fe8cb48cbfa37e1e
Fredde420 <fredrik.helleblad@skola.taby.se>
2025-01-08 14:32:16 +0100

    Beskrivning monster

--- src/Monster.java
+++ src/Monster.java
@@ -1,5 +1,7 @@
 // Base class for monsters
+
 public abstract class Monster {
+
     private String name;
     private int health;
     private int attackPower;
@@ -36,22 +38,37 @@ public abstract class Monster {
 
 // Dragon class
 class Dragon extends Monster {
+
     public Dragon(String name, int health, int attackPower) {
         super(name, health, attackPower);
+    }
+
+    public String getDescription() {
+        return "A fearsome dragon with fiery breath and immense strength. It soars high in the skies, guarding its treasure hoard and striking fear into the hearts of all who dare challenge it.";
     }
 }
 
 // Goblin class
 class Goblin extends Monster {
+
     public Goblin(String name, int health, int attackPower) {
         super(name, health, attackPower);
+    }
+
+    public String getDescription() {
+        return "A sneaky goblin, small in size but very greedy and cunning. They often lurk in dark caves and ambush unsuspecting travelers to steal their gold.";
     }
 }
 
 // Troll class
 class Troll extends Monster {
+
     public Troll(String name, int health, int attackPower) {
         super(name, health, attackPower);
+    }
+
+    public String getDescription() {
+        return "A massive troll with thick skin and crushing strength. Known for their regenerative abilities, trolls are formidable foes who inhabit desolate mountains, forests and dungeons.";
     }
 }
 
@@ -61,11 +78,14 @@ class Troll extends Monster {
 //         Dragon dragon = new Dragon("Red Dragon", 200, 50);
 //         Goblin goblin = new Goblin("Sneaky Goblin", 100, 20);
 //         Troll troll = new Troll("Mountain Troll", 150, 40);
-
+// System.out.println(dragon.getName() + ": " + dragon.getDescription());
+//         System.out.println(goblin.getName() + ": " + goblin.getDescription());
+//         System.out.println(troll.getName() + ": " + troll.getDescription());
+//     
 //         dragon.attack(goblin);
 //         goblin.attack(troll);
 //         troll.attack(dragon);
-
+// 
 //         System.out.println(goblin.getName() + " has " + goblin.getHealth() + " health left.");
 //         System.out.println(troll.getName() + " has " + troll.getHealth() + " health left.");
 //         System.out.println(dragon.getName() + " has " + dragon.getHealth() + " health left.");
